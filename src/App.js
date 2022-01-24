4c7872c29471cc3bf642ba7d20aa01a9c8682dba
Pitel Francois <francoispitel87@gmail.com>
2022-01-24 17:34:42 +0100

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
